971bade03d890397de7f8ee9d5bc1c914bc30c7f
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-01-19 22:36:37 -0300

    :card_file_box: route /games

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,24 @@ import express from 'express';
 import { PrismaClient } from '@prisma/client';
 
 const app = express();
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  log: ['query']
+});
 
 app.use(express.json());
 
-app.get('/games', (request, response) => {
-  return response.json([]);
+app.get('/games', async (request, response) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true,
+        }
+      }
+    }
+  });
+
+  return response.json(games);
 });
 
 app.post('/ads', (request, response) => {
