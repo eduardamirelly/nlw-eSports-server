89860fda03172432bdfb640434795a226582225a
eduardamirelly <mirelly.eduarda404@gmail.com>
2023-01-19 21:56:43 -0300

    :card_file_box: add prisma and run first migrate with Game model

--- src/server.ts
+++ src/server.ts
@@ -4,8 +4,22 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (request, response) => {
-  return response.send('Hello World');
+app.get('/games', (request, response) => {
+  return response.json([]);
+});
+
+app.post('/ads', (request, response) => {
+  return response.status(201).json([]);
+});
+
+app.get('/games/:id/ads', (request, response) => {
+  return response.json([
+    { id: 1, name: 'Anúncio 1' },
+    { id: 2, name: 'Anúncio 2' },
+    { id: 3, name: 'Anúncio 3' },
+    { id: 4, name: 'Anúncio 4' },
+    { id: 5, name: 'Anúncio 5' },
+  ]);
 });
 
 app.listen(3333);
